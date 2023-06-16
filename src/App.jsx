fa4e6b99db103062ba8ec2b0c3210350ee687103
alimov m <jj24ers@gmail.com>
2023-06-16 17:59:24 +0600

    makes every component work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-function App() {
-  
+import React, { useState } from "react";
+import ClassSelector from "./ClassSelector";
+import Schedule from "./Schedule";
+import Timetable from "./Timetable";
 
-}
+const App = () => {
+  const [classAll, setClassAll] = useState("");
 
-export default App
+  const handleClassSelect = (selectedClass) => {
+    setClassAll(selectedClass);
+  };
+
+  return (
+    <div>
+      <ClassSelector onClassSelect={handleClassSelect} />
+      <Schedule classAll={classAll} />
+      <Timetable />
+    </div>
+  );
+};
+
+export default App;
