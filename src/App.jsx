4d3541ef08cedec4e5143b29ab51919088e10abf
alimov m <jj24ers@gmail.com>
2023-06-26 19:32:13 +0600

    breaks  everything

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,179 @@ import Schedule from "./Schedule";
 import Timetable from "./Timetable";
 import { Button } from "antd";
 
+const lessons = [
+  [
+    [
+      {
+        color: "#A5DEF2",
+        id: [0],
+        duration: 2,
+        starttime: "08:00",
+        endtime: "9:30",
+        classroomids: ["339"],
+      },
+      {
+        color: "#5BAEB7",
+        id: [1],
+        duration: 2,
+        starttime: "09:40",
+        endtime: "11:05",
+        classroomids: ["202"],
+      },
+      {
+        color: "#1E80C1",
+        id: [2],
+        duration: 2,
+        starttime: "11:10",
+        endtime: "12:50",
+        classroomids: ["218"],
+      },
+      {
+        color: "#414C6B",
+        id: [3],
+        duration: 2,
+        starttime: "13:10",
+        endtime: "14:50",
+        classroomids: ["126"],
+      },
+      {
+        color: "#067FD0",
+        id: [4],
+        duration: 2,
+        starttime: "14:55",
+        endtime: "16:25",
+        classroomids: ["217"],
+      },
+    ],
+    [
+      {
+        color: "#A5DEF2",
+        id: [0],
+        duration: 2,
+        starttime: "08:00",
+        endtime: "9:30",
+        classroomids: ["339"],
+      },
+      {
+        color: "#5BAEB7",
+        id: [1],
+        duration: 2,
+        starttime: "09:40",
+        endtime: "11:05",
+        classroomids: ["202"],
+      },
+      {
+        color: "#1E80C1",
+        id: [2],
+        duration: 2,
+        starttime: "11:10",
+        endtime: "12:50",
+        classroomids: ["218"],
+      },
+      {
+        color: "#414C6B",
+        id: [3],
+        duration: 2,
+        starttime: "13:10",
+        endtime: "14:50",
+        classroomids: ["126"],
+      },
+      {
+        color: "#067FD0",
+        id: [4],
+        duration: 2,
+        starttime: "14:55",
+        endtime: "16:25",
+        classroomids: ["217"],
+      },
+    ],
+    [
+      {
+        color: "#A5DEF2",
+        id: [0],
+        duration: 2,
+        starttime: "08:00",
+        endtime: "9:30",
+        classroomids: ["339"],
+      },
+      {
+        color: "#5BAEB7",
+        id: [1],
+        duration: 2,
+        starttime: "09:40",
+        endtime: "11:05",
+        classroomids: ["202"],
+      },
+      {
+        color: "#1E80C1",
+        id: [2],
+        duration: 2,
+        starttime: "11:10",
+        endtime: "12:50",
+        classroomids: ["218"],
+      },
+      {
+        color: "#414C6B",
+        id: [3],
+        duration: 2,
+        starttime: "13:10",
+        endtime: "14:50",
+        classroomids: ["126"],
+      },
+      {
+        color: "#067FD0",
+        id: [4],
+        duration: 2,
+        starttime: "14:55",
+        endtime: "16:25",
+        classroomids: ["217"],
+      },
+    ],
+    [
+      {
+        color: "#A5DEF2",
+        id: [0],
+        duration: 2,
+        starttime: "08:00",
+        endtime: "9:30",
+        classroomids: ["339"],
+      },
+      {
+        color: "#5BAEB7",
+        id: [1],
+        duration: 2,
+        starttime: "09:40",
+        endtime: "11:05",
+        classroomids: ["202"],
+      },
+      {
+        color: "#1E80C1",
+        id: [2],
+        duration: 2,
+        starttime: "11:10",
+        endtime: "12:50",
+        classroomids: ["218"],
+      },
+      {
+        color: "#414C6B",
+        id: [3],
+        duration: 2,
+        starttime: "13:10",
+        endtime: "14:50",
+        classroomids: ["126"],
+      },
+      {
+        color: "#067FD0",
+        id: [4],
+        duration: 2,
+        starttime: "14:55",
+        endtime: "16:25",
+        classroomids: ["217"],
+      },
+    ],
+  ],
+];
+
 const App = () => {
   const [classAll, setClassAll] = useState("");
 
@@ -12,9 +185,9 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div class={{ backgrondColor: "#808080" }}>
       <ClassSelector onClassSelect={handleClassSelect} />
-      <Schedule classAll={classAll} />
+      <Schedule lessons={lessons} />
       <Timetable />
       <Button
         type="primary"
--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const Schedule = ({ classAll }) => {
-  return <p>Your class is: {classAll}</p>;
+const lessonsId = ["ruso", "ingles", "matematicas", "fisica", "literatura"];
+
+const Week = ({ week }) => {
+  console.log(week);
+  return (
+    <tr>
+      <td
+        style={{
+          backgroundColor: "#414C6B",
+          width: "200px",
+          height: "200px",
+        }}
+      >
+        {week[0][0].color}
+      </td>
+      <td>Alfreds Futterkiste</td>
+      <td>Maria Anders</td>
+      <td>Germany</td>
+    </tr>
+  );
+};
+
+const Schedule = ({ lessons }) => {
+  const [week1, week2, week3, week4] = lessons;
+
+  return (
+    <table style={{ color: "white" }}>
+      <Week week={week1} />
+      <Week week={week2} />
+      <Week week={week3} />
+      <Week week={week4} />
+    </table>
+  );
 };
 
 export default Schedule;
